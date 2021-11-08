10437cff8d0535c2b92c803409f05941c6cc1288
gustavo.goncalves <gustavo.goncalves@uem.com.br>
2021-11-08 18:40:27 -0300

    auth middleware, public and private routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,19 @@
 import { Router } from "express"
 import { CreateUserController } from "./controllers/CreateUserController"
 import { LoginController } from "./controllers/LoginController"
+import { auth } from "./middlewares/auth"
 
 const routes = Router()
 
 routes.post("/register", new CreateUserController().handle)
 routes.post("/login", new LoginController().handle)
+
+routes.get("/private", [auth()], (req, res) => {
+  res.send("Private route")
+})
+
+routes.get("/public", (req, res) => {
+  res.send("Public route")
+})
 
 export { routes }
