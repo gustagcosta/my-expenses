9db0fce9c90102d40081bcfe3c1325dae5a58601
gustavo.goncalves <gustavo.goncalves@uem.com.br>
2021-11-08 18:48:22 -0300

    error and not found handlers

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,12 +1,16 @@
 import "./dotenv"
 import express from "express"
 import { routes } from "./routes"
+import { errorHandler, notFound } from "./middlewares/error"
 
 const app = express()
 
 app.use(express.json())
 
 app.use("/api/v1", routes)
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(3333, () => {
   console.log("server running...")
